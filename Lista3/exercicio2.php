d7bcef911dea01c708756e60dbbd8a8d0e31366a
bruno <freitas.bruno@aluno.ifsp.edu.br>
2022-04-19 22:16:50 -0300

    Listas

--- Lista3/exercicio2.php
+++ Lista3/exercicio2.php
@@ -1,9 +1,17 @@
 <?php
     $a = "Paralelepipedo";
+    $P = strlen($a);
     
-    /*if ($a = "a". "e". "i". "o". "u") {
-        $a = "a" == "x";
-    }*/
+    for($i=0; $i < $P; $i++) {
+        switch ($a[$i]) {
+            case 'a': $a[$i] = "x"; break;
+            case 'e': $a[$i] = "x"; break;
+            case 'i': $a[$i] = "x"; break;
+            case 'o': $a[$i] = "x"; break;
+            case 'u': $a[$i] = "x"; break;
+            default: $a[$i] = $a[$i]; break;
+        }
+    }
 
     echo $a;
 ?>
