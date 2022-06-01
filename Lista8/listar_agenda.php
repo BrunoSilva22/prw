3da72299c27d6a58147dccd8503cf12814963b15
Bruno <freitas.bruno@aluno.ifsp.edu.br>
2022-06-01 11:42:12 -0300

    lista8

--- Lista8/listar_agenda.php
+++ Lista8/listar_agenda.php
@@ -34,7 +34,6 @@
             while ($row = mysqli_fetch_array($result)){
                 echo "<tr>";
                 echo "<td>" . $row['id_agenda'] . "</td>";
-                echo "<td><img src='data:image/jpeg;base64,".base64_encode( $row["foto_blob"] )."' width='150' height='150'/></td>";
                 echo "<td><a href='altera_agenda.php?id_agenda=".$row['id_agenda']."'>".$row['nome']. "</a></td>";
                 echo "<td>" . $row['apelido'] . "</td>";
                 echo "<td>" . $row['endereco'] . "</td>";
@@ -45,6 +44,7 @@
                 echo "<td>" . $row['celular'] . "</td>";
                 echo "<td>" . $row['email'] . "</td>";
                 echo "<td>" . $row['dt_cadastro'] . "</td>";
+                echo "<td><img src='data:image/jpeg;base64,".base64_encode( $row["foto_blob"] )."' width='150' height='150'/></td>";
                 echo "<td><a href='excluir_agenda.php?id_agenda=".$row['id_agenda']."'>Excluir</a></td>";
                 echo "</tr>";
             }
